b37efe98061fcc49ceeccda8885c280ab737972d
Antonio Montalvo <ant.mon.diaz@hotmail.com>
2024-06-11 20:31:24 -0600

    fixing tittle

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   const [currentDate, setCurrentDate] = useState(new Date());
   const [steps, setSteps] = useState(0);
-  const [count, setCount] = useState(0);
+  const [days, setDays] = useState(0);
 
   const valueUp = (currentValue, addend) => {
     return +currentValue + +addend;
@@ -22,35 +22,35 @@ function App() {
   };
 
   const counterUp = () => {
-    setCount(valueUp(count, steps === 0 ? 1 : steps));
+    setDays(valueUp(days, steps === 0 ? 1 : steps));
     updateDate();
   };
 
   const counterDown = () => {
-    setCount(valueDown(count, steps === 0 ? 1 : steps));
+    setDays(valueDown(days, steps === 0 ? 1 : steps));
     updateDate();
   };
 
   const updateDate = () => {
     setCurrentDate(
       (currentDate) =>
-        new Date(currentDate.getTime() + count * 24 * 60 * 60 * 1000)
+        new Date(currentDate.getTime() + days * 24 * 60 * 60 * 1000)
     );
   };
 
   return (
     <div className="App">
-      <p>Clock Counter</p>
+      <p>Date Counter</p>
       <div>
         <button onClick={stepDown}>-</button>
         Step: {steps}
         <button onClick={stepUp}>+</button>
         <p />
         <button onClick={counterDown}>-</button>
-        Count: {count}
+        Count: {days}
         <button onClick={counterUp}>+</button>
       </div>
-      <p>{"" + currentDate}</p>
+      <p>{days + " days from today is " + currentDate.toDateString()}</p>
     </div>
   );
 }
