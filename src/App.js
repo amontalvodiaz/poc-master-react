e0e55ab6865b6d5e3711b78b98acbc019f7c5365
Antonio Montalvo <ant.mon.diaz@hotmail.com>
2024-06-11 20:30:31 -0600

    challenge:  date counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [steps, setSteps] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const valueUp = (currentValue, addend) => {
+    return +currentValue + +addend;
+  };
+
+  const valueDown = (currentValue, addend) => {
+    return +currentValue - +addend;
+  };
+
+  const stepUp = () => {
+    setSteps(valueUp(steps, 1));
+  };
+
+  const stepDown = () => {
+    setSteps(valueDown(steps, 1));
+  };
+
+  const counterUp = () => {
+    setCount(valueUp(count, steps === 0 ? 1 : steps));
+    updateDate();
+  };
+
+  const counterDown = () => {
+    setCount(valueDown(count, steps === 0 ? 1 : steps));
+    updateDate();
+  };
+
+  const updateDate = () => {
+    setCurrentDate(
+      (currentDate) =>
+        new Date(currentDate.getTime() + count * 24 * 60 * 60 * 1000)
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Clock Counter</p>
+      <div>
+        <button onClick={stepDown}>-</button>
+        Step: {steps}
+        <button onClick={stepUp}>+</button>
+        <p />
+        <button onClick={counterDown}>-</button>
+        Count: {count}
+        <button onClick={counterUp}>+</button>
+      </div>
+      <p>{"" + currentDate}</p>
     </div>
   );
 }
